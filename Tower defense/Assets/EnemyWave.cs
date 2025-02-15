950825b028068b9f2522b5971cc9fd96413944ba
Pyotr <pyotrk@inbox.lv>
2025-02-15 13:56:33 +0200

    Added new scripts
    Enemy Wave Manager
    Enemy Wave

---
+++ Tower defense/Assets/EnemyWave.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+namespace TowerDefence
+{
+    public class EnemyWave: MonoBehaviour
+    {
+        [Serializable]
+        private class Squad
+        {
+            public EnemyAsset asset;
+            public int count;
+        }
+
+        [Serializable]
+        private class PathGroup
+        {
+            public Squad[] squads;
+        }
+
+        [SerializeField] private PathGroup[] groups;
+
+        [SerializeField] private float prepareTime = 10f;
+
+        private void Awake()
+        {
+            enabled = false;
+        }
+
+        private event Action OnWaveReady;
+
+        public void Prepare(Action spawnEnemies)
+        {
+            prepareTime += Time.time;
+            enabled = true;
+            OnWaveReady += spawnEnemies;
+        }
+
+        private void Update()
+        {
+            if (Time.time >= prepareTime)
+            {
+                enabled = false;
+                OnWaveReady?.Invoke();
+            }
+        }
+
+        public IEnumerable<(EnemyAsset asset, int count, int pathIndex)> EnumerateSquads()
+        {
+            yield return (groups[0].squads[0].asset, groups[0].squads[0].count, 0);
+        }
+
+        internal EnemyWave PrepareNext(Action spawnEnemies)
+        {
+            return null;
+        }
+    }
+}
---
+++ Tower defense/Assets/EnemyWaveManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace TowerDefence
+{
+    public class EnemyWaveManager : MonoBehaviour
+    {
+        [SerializeField] private Enemy m_EnemyPrefab;
+        [SerializeField] private Path[] paths;
+        [SerializeField] private EnemyWave currentWave;
+
+        private void Start()
+        {            
+            currentWave.Prepare(SpawnEnemies);
+
+        }
+
+        private void SpawnEnemies()
+        {
+            foreach ((EnemyAsset asset, int count, int pathIndex) in currentWave.EnumerateSquads())
+            {
+                if (pathIndex < paths.Length)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        var e = Instantiate(m_EnemyPrefab);
+                        e.Use(asset);
+                        e.GetComponent<TDPatrolController>().SetPath(paths[pathIndex]);
+                    }
+                                                        
+                }
+                else
+                {
+                    Debug.LogWarning($"Invalid pathIndex in {name}");
+                }                
+            }
+                        
+            currentWave = currentWave.PrepareNext(SpawnEnemies);            
+        }
+    }
+}
+
